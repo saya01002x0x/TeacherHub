fa4600b86d27e9a17d364f96e83a92786c447b84
nubiz24 <pduong2924@gmail.com>
2025-11-22 23:11:18 +0700

    feat: authprovide ver1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Routes, Route, Navigate } from "react-router";
+import * as Sentry from "@sentry/react";
+import { useAuth } from "./context/AuthContext";
 
-function App() {
-  return (
-    <>
-    </>
-  )
-}
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
-export default App
+const App = () => {
+    const { currentUser } = useAuth();
+
+    return (
+        <>
+            <div style={{ padding: 20 }}>
+                <h1>Testing Mock User</h1>
+                <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+            </div>
+            {/*<SignedIn>*/}
+            {/*    <SentryRoutes>*/}
+            {/*        <Route path="/" element={<HomePage />} />*/}
+            {/*        <Route path="/auth" element={<Navigate to={"/"} replace />} />*/}
+            {/*    </SentryRoutes>*/}
+            {/*</SignedIn>*/}
+
+            {/*<SignedOut>*/}
+            {/*    <SentryRoutes>*/}
+            {/*        <Route path="/auth" element={<AuthPage />} />*/}
+            {/*        <Route path="*" element={<Navigate to={"/auth"} replace />} />*/}
+            {/*    </SentryRoutes>*/}
+            {/*</SignedOut>*/}
+        </>
+    );
+};
+
+export default App;
---
+++ frontend/src/mock/users.js
@@ -0,0 +1,17 @@
+export const MOCK_USERS = [
+    {
+        id: 1,
+        name: "User A",
+        avatar: "https://i.pravatar.cc/150?img=1"
+    },
+    {
+        id: 2,
+        name: "User B",
+        avatar: "https://i.pravatar.cc/150?img=2"
+    },
+    {
+        id: 3,
+        name: "User C",
+        avatar: "https://i.pravatar.cc/150?img=3"
+    }
+];
